738db078717224c253960144c54930869e47fb4b
Lebannen <chan_kai@outlook.com>
2022-04-09 21:22:45 +0200

    add second input

--- input/input.ino
+++ input/input.ino
@@ -1,18 +1,20 @@
 #include <IRremote.h>
 
 int receiver = 11;
-int commands[5] = {0, 3, 0, 0, 0};
+int commands1[5] = {0, 0, 0, 0, 0};
+int commands2 = commands1;
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Beginn IR receiving.");
   IrReceiver.begin(receiver, ENABLE_LED_FEEDBACK);
-  Serial.println("Press the buttons you want to use as controls on your remote in this order: up, down, left, right, enter.");
+  Serial.println("Press the buttons you want to use as controls on your remote in this order: up, down, left, right, enter. Firsts Player One, then Player Two");
   set();
-  for (int i = 0; i == 5; i++)
+  for (int i = 0; i < 5; i++)
   {
-    Serial.println(commands[i]);
+    Serial.println(commands1[i]);
+    Serial.println(commands2[i]);
   } 
 }
 
@@ -32,11 +34,21 @@ void input()
 
 void set()
 {
-  for (int i = 0; i == 5; i++)
+  int i = 0;
+  while (i < sizeof(commands1)/sizeof(int) * 2)
   {
     if (IrReceiver.decode())
     {
-      commands[i] = IrReceiver.decodedIRData.command;
+      int command = IrReceiver.decodedIRData.command;
+      if (i > 0 && commands1[i-1] == command)
+      {
+        i --;
+      }
+      if i == 0 or i % 2 == 0:
+        commands1[i] = command;
+      else:
+        commands2[i - 1] = command;
+      i ++;
       IrReceiver.resume();
     }
   }
