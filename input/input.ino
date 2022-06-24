b9d850ea5eaf7f87ba413356841a47874e98c478
Lebannen <chan_kai@outlook.com>
2022-06-24 20:01:31 +0200

    added check for same values within on eplayer's commnads

--- input/input.ino
+++ input/input.ino
@@ -1,12 +1,12 @@
-// writes commands to the serial port corresponding to IR inputs chosen by two players using remotes
+  // writes commands to the serial port corresponding to IR inputs chosen by two players using remotes
 #include <IRremote.h>
 
 /*
   pinning for IR receiver:
   facing front:
-  - left: 5V
+  - left: 11
   - middle: GND
-  - right: 11
+  - right: 5v
   pinning for RGB LED (common cathode)
   facing front(marker mark), left to right:
   - 1: 3 (R)
@@ -16,7 +16,7 @@
 */
 
 // set pins and variables
-const byte RECEIVER_PIN = 11;
+const byte RECEIVER_PIN = 12;
 const byte LED_R_PIN = 3;
 const byte LED_G_PIN = 5;
 const byte LED_B_PIN = 6;
@@ -34,7 +34,6 @@ int commands_P2[5];
   center|  15 |  25 |
 */
 
-bool firstSet = true;
 byte input;
 
 
@@ -46,7 +45,7 @@ void setup() {
 
   // begin serial port and IR receiving
   Serial.begin(115200);
-  IrReceiver.begin(RECEIVER_PIN);
+  IrReceiver.begin(RECEIVER_PIN, ENABLE_LED_FEEDBACK);
   Serial.println("IR receiving has begun.");
 
   // set IR command storage arrays
@@ -54,6 +53,9 @@ void setup() {
     set(commands_P1, 1);
     set(commands_P2, 2);
   } while (!check());
+  
+  rgbProtocol("confirm");
+  Serial.println("\nSetup complete, ready to take players' inputs");
 }
 
 
@@ -92,7 +94,10 @@ void loop() {
     else if (IrReceiver.decodedIRData.command == commands_P2[4]) {
       input = 25;
     }
-    Serial.write(input);
+    if (input != 0) {
+      Serial.println(input);      
+    }
+    IrReceiver.resume();
   }
 }
 
@@ -103,9 +108,9 @@ void set(int commands[], int p) {
   rgbProtocol("set");
 
   if (p == 1) {
-    Serial.println("Player One:");
+    Serial.println("\nPlayer One:");
   } else {
-    Serial.println("Player Two:");
+    Serial.println("\nPlayer Two:");
   }
 
   Serial.println("Press the buttons you want to use as controls on your remote in this order: up, down, left, right, enter.");
@@ -125,40 +130,56 @@ void set(int commands[], int p) {
     }
   }
 
+  delay(2000);
+
   if (p == 1) {
-    Serial.print("Setting complete, Player One's commands are: ");
+    Serial.println("Setting complete, Player One's commands are: ");
   } else {
-    Serial.print("Setting complete, Player Two's commands are: ");
+    Serial.println("Setting complete, Player Two's commands are: ");
   }
 
   for (int i = 0; i < 5; i++) {
-    Serial.print(commands[i]);
+    Serial.print(F("input "));
+    Serial.print(i + 1);
+    Serial.print(F(": "));
+    Serial.println(commands[i]);
   }
 }
 
 
-bool check() {
-  if (!firstSet) {
-    rgbProtocol("error");
-    Serial.println("Some input values are not distinct, please check these values and try again: ");
-    for (int i = 0; i < 5; i++) {
-      Serial.println("input ");
-      Serial.print(i + 1);
-      Serial.println("Player One: ");
-      Serial.print(commands_P1[i]);
-      Serial.print(" - Player Two: ");
-      Serial.print(commands_P2[i]);
+bool check() {  
+  for (int i = 0; i < 5; i++) {  
+    if (i < 4 and (commands_P1[i] == commands_P1[i+1] or commands_P2[i] == commands_P2[i+1])) {
+      printCommandError();
+      return false;
     }
-  }
-  
-  for (int i = 0; i < 5; i++) {
+    else if (i == 4 and (commands_P1[i] == commands_P1[0] or commands_P2[i] == commands_P2[0])) {     
+      printCommandError();
+      return false;
+    }
+   
     for (int j = 0; j < 5; j++) {
       if (commands_P1[i] == commands_P2[j]) {
+        printCommandError();
         return false;
       }
-    }
+    } 
   }
   return true;
+}
+
+
+void printCommandError() {
+  rgbProtocol("error");
+  Serial.println("\nSome input values are not distinct, please check these values and try again: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.print(F("input "));
+    Serial.println(i + 1);
+    Serial.print(F("Player One: "));
+    Serial.print(commands_P1[i]);
+    Serial.print(F(" - Player Two: "));
+    Serial.println(commands_P2[i]);
+  }
 }
 
 
@@ -166,16 +187,27 @@ void rgbProtocol(String protocol) {
   if (protocol == "set") {
     for (int i = 0; i < 3; i++) {
       analogWrite(LED_B_PIN, 255);
-      delay(10);
+      delay(500);
       analogWrite(LED_B_PIN, 0);  
+      delay(500);
     }
   }
   
   else if (protocol == "error") {
     for (int i = 0; i < 3; i++) {
       analogWrite(LED_R_PIN, 255);
-      delay(10);
+      delay(500);
       analogWrite(LED_R_PIN, 0);  
+      delay(500);      
+    }
+  }
+
+  else if (protocol == "confirm") {
+    for (int i = 0; i < 3; i++) {
+      analogWrite(LED_G_PIN, 255);
+      delay(500);
+      analogWrite(LED_G_PIN, 0);  
+      delay(500);
     }
   }
 }
